a3ca627a3a6c58ef4dac27a2e1624f8940c38380
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-12 18:25:27 +0200

    Annotated all the endpoints in the BeerController with validation annotations.
    Implemented Controller advice to handle globally errors.

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -4,36 +4,43 @@ import guru.springframework.msscbeerservice.web.model.BeerDto;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
 
+//todo: in case of validation problems - refer to Video #70 and the note for the validation annotations
+
+@Validated
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
+    public ResponseEntity<BeerDto> getBeerById(@NotNull @PathVariable UUID beerId) {
 
         //todo impl
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> saveNewBeer(@NotNull @Valid @RequestBody BeerDto beerDto) {
 
         //todo impl
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity<BeerDto> updateBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> updateBeer(@NotNull  @PathVariable UUID beerId, @NotNull @Valid @RequestBody BeerDto beerDto) {
 
         //todo impl
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
     }
-
 
 }
