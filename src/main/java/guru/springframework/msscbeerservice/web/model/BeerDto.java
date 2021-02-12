e5d1ecd787719b90694ff6319856d6072e62274c
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-12 18:09:27 +0200

    Added validation constraints for the BeerDto.
    Updated the pom.xml with validation starter.

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -2,6 +2,10 @@ package guru.springframework.msscbeerservice.web.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -12,14 +16,32 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
+
+    @Null
     private Integer version;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
 
 
