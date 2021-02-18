794627247002899b21da784775e8aeb445769d53
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-02-18 16:40:02 +0200

    Implemented BrewingService to trigger brewing.

--- src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryDto.java
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryDto.java
@@ -1,4 +1,4 @@
-package guru.springframework.msscbeerservice.services.inventory.model;
+package guru.springframework.msscbeerservice.services.inventory;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
